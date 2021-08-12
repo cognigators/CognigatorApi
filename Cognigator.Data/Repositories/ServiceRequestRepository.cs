46c2a59148696e64e4e9515f9e6a849711386da4
kamed <kameswaran.d@vixxo.com>
2021-08-12 17:20:57 +0530

    Created Service Controller

---
+++ Cognigator.Data/Repositories/ServiceRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using Cognigator.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cognigator.Data.Repositories
+{
+    public class ServiceRequestRepository: IServiceRequestRepository
+    {
+    }
+}
---
+++ CognigatorApi/Controllers/ServiceRequestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CognigatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceRequestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "values 1", "values 2" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "The value is" + id;
+        }
+    }
+}
